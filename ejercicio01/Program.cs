5c8539191d6d99da9123b1090c208f8e79a8af1a
Cristian Cordoba <cristian56cordoba@gmail.com>
2024-05-14 12:52:02 -0300

    finalizacion de ejercicio1

--- ejercicio01/Program.cs
+++ ejercicio01/Program.cs
@@ -1,1 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+int a = 12344;
+int aux = a;
+int dig=0;
+int numfin=0;
+while (aux % 10 != 0)
+{
+    dig = aux%10;
+    a = a/10;
+    aux = a;
+    numfin = numfin*10 + dig;
+}
+
+Console.WriteLine(numfin);
